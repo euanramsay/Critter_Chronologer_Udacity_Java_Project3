7f5cccaf1f655499e446d97384b7f6c0601be91b
Euan Ramsay <euan@Euans-MacBook-Pro.local>
2021-01-31 16:10:58 +0000

    Set up basic entity structure and start repository.

---
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Customer.java
@@ -0,0 +1,16 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
+
+
---
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
@@ -0,0 +1,15 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
+
