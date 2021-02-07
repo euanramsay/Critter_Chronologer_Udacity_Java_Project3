effe4317bfa7b5b59564f46dc29e1706e7d694dc
Euan Ramsay <euan@Euans-MacBook-Pro.local>
2021-02-07 14:53:00 +0000

    Add Pet and Schedule entity config

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -26,11 +26,58 @@ public class Pet {
 
     private LocalDate birthDate;
 
-    @Column(length = 500)
     private String notes;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
 
--- src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
@@ -1,8 +1,17 @@
 package com.udacity.jdnd.course3.critter.entity;
 
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Schedule {
@@ -10,5 +19,62 @@ public class Schedule {
     @Id
     @GeneratedValue
     private Long id;
+
+    private LocalDate date;
+
+    @ManyToMany
+    @JoinTable(name = "schedule_employee",
+            joinColumns = @JoinColumn(name = "schedule_id"),
+            inverseJoinColumns = @JoinColumn(name = "employee_id"))
+    private List<Employee> employees;
+
+    @ManyToMany
+    @JoinTable(name = "schedule_pet",
+            joinColumns = @JoinColumn(name = "schedule_id"),
+            inverseJoinColumns = @JoinColumn(name = "pet_id"))
+    private List<Pet> pets;
+
+    @ElementCollection
+    private Set<EmployeeSkill> activities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
 }
 
