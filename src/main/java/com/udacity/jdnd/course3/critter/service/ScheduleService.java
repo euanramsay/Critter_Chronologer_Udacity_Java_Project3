a96086758c3f4c8ceb9aa8a92a76457a73cfd198
Euan Ramsay <euan@Euans-MacBook-Pro.local>
2021-02-07 15:44:02 +0000

    Setting up schedule service

--- src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -1,7 +1,26 @@
 package com.udacity.jdnd.course3.critter.service;
 
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
+import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
+import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import com.udacity.jdnd.course3.critter.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScheduleService {
+
+    private ScheduleRepository scheduleRepository;
+
+    private EmployeeRepository employeeRepository;
+
+    private PetRepository petRepository;
+
+    private CustomerRepository customerRepository;
+
+    public List<Schedule> findAllSchedules() {
+        return scheduleRepository.findAll();
+    }
 }
