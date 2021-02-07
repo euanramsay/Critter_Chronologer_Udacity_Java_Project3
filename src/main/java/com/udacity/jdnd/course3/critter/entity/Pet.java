75ebae6cfd0d0a965ef8d6e312f4fa7cf24a5264
Euan Ramsay <euan@Euans-MacBook-Pro.local>
2021-02-07 13:45:24 +0000

    Setting up Entity for Customer, Employee and Pet

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -1,15 +1,36 @@
 package com.udacity.jdnd.course3.critter.entity;
 
+import com.udacity.jdnd.course3.critter.pet.PetType;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
 
 @Entity
 public class Pet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
+
+    private PetType petType;
+
+    @Nationalized
+    private String name;
+
+    private LocalDate birthDate;
+
+    @Column(length = 500)
+    private String notes;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
 
--- src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
@@ -2,14 +2,13 @@ package com.udacity.jdnd.course3.critter.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Schedule {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 }
 
